94d11b1f0891411133704605326f281d46b4ed3e
v_dmitrov <v.dmitrov@smartmerch.it>
2024-12-29 01:30:00 +0300

    clean code

--- backend/errors/AlreadyExists.js
+++
@@ -1,9 +0,0 @@
-class AlreadyExists extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'AlreadyExists';
-    this.status = 409;
-  }
-}
-
-module.exports = AlreadyExists;
--- backend/errors/AuthorizationError.js
+++
@@ -1,9 +0,0 @@
-class AuthorizationError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'AuthorizationError';
-    this.status = 401;
-  }
-}
-
-module.exports = AuthorizationError;
